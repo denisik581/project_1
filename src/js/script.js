3f6ee9193eea9fd2371ca877474fc8e2e11774f5
denis <drakonniravgol@gmail.com>
2020-03-18 18:37:07 +0300

    add + adaptation modal

--- src/js/script.js
+++ src/js/script.js
@@ -39,4 +39,20 @@ $(document).ready(function(){
     toggleSlide('.catalog-item_back');
 
 
+    //modal
+
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('fast');
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('fast');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('fast');
+        });
+    });
+
 }); 

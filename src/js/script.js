d9a53b2bd076cecc555c5668cd7fddd17c9ad9cc
denis <drakonniravgol@gmail.com>
2020-03-18 22:29:53 +0300

    add validation form + add email

--- src/js/script.js
+++ src/js/script.js
@@ -55,4 +55,49 @@ $(document).ready(function(){
         });
     });
 
+    
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Пожалуйста, введите своё имя",
+                phone: "Полажуйста, введите ваш телефон",
+                email: {
+                    required: "Пожалуйста, введите свой email",
+                    email: "ваш email должен быть в формате: name@domain.com"
+                }
+            }
+        });
+    }
+
+    validateForms('#first_form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999 99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('fast');
+
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+    
 }); 
